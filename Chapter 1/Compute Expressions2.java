58eb3388122765a04df23e065fd93bcaeb96555f
minimoose97 <50415614+minimoose97@users.noreply.github.com>
2019-05-09 10:46:52 -0600

    added chapter 1

---
+++ Chapter 1/Compute Expressions2.java
@@ -0,0 +1,15 @@
+/*
+Author: Mark Melling
+Date: 5/9/2018
+
+This code gives the area of the specified rectangle. This code also gives the perimeter of the specified rectangle as well.
+*/
+class ComputeExpressions {
+	public static void main(String[] args) {
+		System.out.println("area of rectangle is");
+		System.out.println(4.5 * 7.9);
+		System.out.println("perimiter of rectangle is");
+		System.out.println((4.5 * 2) + (7.9 * 2));
+		
+	}
+}
---
+++ Chapter 1/ComputeExpression.java
@@ -0,0 +1,11 @@
+/*
+Author: Mark Melling
+Date: 5/9/2018
+
+This code displays the answer to a strictly mathematical equation.
+*/
+public class ComputeExpression {
+	public static void main(String[] args) {
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) /(45.5-3.5));
+	}	
+}
